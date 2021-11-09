d318c2330fd425739d2774ad3db6f07368ff3c81
Caio Mouta <cvmouta01@gmail.com>
2021-11-09 18:05:33 -0300

    lista 1

--- Listas/Lista 1/Caio/1_3.py
+++ Listas/Lista 1/Caio/1_3.py
@@ -1,2 +1,2 @@
-soma = map(float,input('digite 3 numeros(separe com espaços)').split)
+soma = sum(map(int, input('digite 3 numeros inteiros(separe com espaços)').split()))
 print(f'soma: {soma}')
---
+++ Listas/Lista 1/Caio/1_4.py
@@ -0,0 +1,2 @@
+num = float(input('Insira um número: '))
+print(f'Quadrado do seu numero: {num * num}')
