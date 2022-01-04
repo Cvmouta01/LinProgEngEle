8d05233f666d429e36c217c32dae79f28f253703
Caio Mouta <cvmouta01@gmail.com>
2022-01-04 16:55:54 -0300

    Update Main.py

--- Listas/Lista 4/Main.py
+++ Listas/Lista 4/Main.py
@@ -1,7 +1,2 @@
-import platform
-import os
-import openpyxl
-import xlrd
 import pandas as pd
-print(platform.platform())
-print(os.getcwd())
+vasco = pd.read_csv('Índice de Energia Elétrica IEE - Histórico  InfoMoney.csv')
