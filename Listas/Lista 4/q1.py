e2f053a21b56701de40280672a3e549125a605a8
Caio Mouta <cvmouta01@gmail.com>
2022-01-04 23:40:04 -0300

    Update q1.py

---
+++ Listas/Lista 4/q1.py
@@ -0,0 +1,35 @@
+class pessoa:
+
+    def __init__(self, nome, idade, altura):
+        self.__nome = nome
+        self.__idade = idade
+        self.__altura = altura
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_idade(self):
+        return self.__idade
+
+    def get_altura(self):
+        return self.__altura
+
+    def set_nome(self, novo):
+        self.__nome = novo
+
+    def set_idade(self, novo):
+        self.__idade = novo
+
+    def set_altura(self, novo):
+        self.__altura = novo
+
+    def show_data(self,):
+        print(f'\nNome: {self.get_nome()}\nIdade: {self.get_idade()}\nAltura: {self.get_altura()}\n')
+
+
+carlos = pessoa('carlos', 17, 1.75)
+carlos.show_data()
+carlos.set_altura(1.76)
+carlos.set_idade(18)
+carlos.set_nome('carlão')
+carlos.show_data()
